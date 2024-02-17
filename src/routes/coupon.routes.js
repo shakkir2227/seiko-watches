23e00c6aa8873fd1ddf68493b9429208f6f145b3
shakir <shakkir2227@gmail.com>
2024-02-17 13:14:47 +0530

    coupon route error fixed

--- src/routes/coupon.routes.js
+++ src/routes/coupon.routes.js
@@ -11,10 +11,8 @@ const router = Router();
 
 router.use(setUserData)
 
-router.use(isAdmin)
-
-router.route("/view-admin").get(couponViewController.adminView)
-router.route("/add").post(addCouponController)
+router.route("/view-admin").get(isAdmin, couponViewController.adminView)
+router.route("/add").post(isAdmin, addCouponController)
 router.route("/apply").put(applyCouponController)
 
 export default router 
